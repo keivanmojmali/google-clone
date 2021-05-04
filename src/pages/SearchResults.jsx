4a1963a1726366ac6bd877aafce65dacbfd869a1
Keivan Mojmali <mojmalik@gmail.com>
2021-05-04 14:08:00 -0400

    working on it

---
+++ src/pages/SearchResults.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class SearchResults extends React.Component {
+    constructor(props){
+        props(props);
+        this.state = {
+
+        };
+
+    }
+    render(){
+        return(
+            <div className="row">
+                <h1>Hellllo</h1>
+            </div>
+        )
+    }
+}
