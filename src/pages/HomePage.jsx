a8424cb879aa1690e4a07ad00be947fd79586198
Keivan Mojmali <mojmalik@gmail.com>
2021-05-03 20:47:50 -0400

    working on it

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,39 +2,47 @@ import React from 'react';
 import NavBar from './NavBar';
 
 
+
 export default class HomePage extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-
+            searchTerm: '',
         };
+        this.state.onChange = this.state.onChange.bind(this);
     };
-    render(){
-        return(
+    onChange(searchQuerry){
+        const searchTerm = searchQuerry.target.value;
+        this.setState({searchTerm});
+    }
+    render() {
+        return (
             <div className="container-fluid m-0 p-0">
                 <div className="row">
                     <div className="col">
-                        <NavBar  /> 
+                        <NavBar />
                     </div>
                 </div>
                 <div className="row">
                     <div className="col">
                         <div className="row">
                             <div className="col d-flex justify-content-center align-items-center">
-                            <img className='img-fluid w-50' src="http://www.google.com/logos/doodles/2015/googles-new-logo-5078286822539264.3-hp2x.gif" alt="google photo"/>
+                                <img className='img-fluid w-50' src="http://www.google.com/logos/doodles/2015/googles-new-logo-5078286822539264.3-hp2x.gif" alt="google photo" />
                             </div>
                         </div>
                         <div className="row">
                             <div className="col d-flex justify-content-center align-items-center">
                                 <form action="#" className='search-bar'>
-                                    <input type="text" className='w-100'/>
+                                    <input type="text" className='w-100' value={this.state.searchTerm} 
+                                    onChange={this.state.onChange}
+                                    />
+                                    <div className="row">
+                                        <div className="col d-flex justify-content-center align-items-center">
+                                            <button type='submit' className='btn btn-sm btn-muted p-1 m-2'> Google Search</button>
+                                            <button type='button' className='btn btn-sm btn-muted p-1 m-2'>I'm Feeling Lucky</button>
+                                        </div>
+                                    </div>
                                 </form>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col d-flex justify-content-center align-items-center">
-                                <button type='button' className='btn btn-sm btn-muted p-1 m-2'> Google Search</button>
-                                <button type='button' className='btn btn-sm btn-muted p-1 m-2'>I'm Feeling Lucky</button>
                             </div>
                         </div>
                         <div className="row m-3">
