6b96e750a53ed922acd14bc25c233f0a7ed3afc6
Keivan Mojmali <mojmalik@gmail.com>
2021-05-03 21:10:41 -0400

    working on it

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,8 +9,25 @@ export default class HomePage extends React.Component {
         this.state = {
             searchTerm: '',
         };
-        this.state.onChange = this.state.onChange.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     };
+    handleSubmit(event){
+        event.preventDefault();
+        console.log(this.state.searchTerm);
+
+        fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyAC_94jn8Bj1CWIaCnmf55ya61W3QOHAFI&cx=60e7b95840ba1222b&q=${this.state.searchTerm}`)
+        .then(result => {
+            return result.json();
+        })
+        .then(data =>{
+            console.log(data,data.rows)
+
+        })
+        .catch( err=> {
+            console.error(err);
+        })
+    }
     onChange(searchQuerry){
         const searchTerm = searchQuerry.target.value;
         this.setState({searchTerm});
@@ -27,18 +44,18 @@ export default class HomePage extends React.Component {
                     <div className="col">
                         <div className="row">
                             <div className="col d-flex justify-content-center align-items-center">
-                                <img className='img-fluid w-50' src="http://www.google.com/logos/doodles/2015/googles-new-logo-5078286822539264.3-hp2x.gif" alt="google photo" />
+                                <img className='img-fluid w-50' src="http://www.google.com/logos/doodles/2015/googles-new-logo-5078286822539264.3-hp2x.gif" alt="google logo" />
                             </div>
                         </div>
                         <div className="row">
                             <div className="col d-flex justify-content-center align-items-center">
-                                <form action="#" className='search-bar'>
+                                <form action="#" className='search-bar' >
                                     <input type="text" className='w-100' value={this.state.searchTerm} 
-                                    onChange={this.state.onChange}
+                                    onChange={this.onChange}
                                     />
                                     <div className="row">
                                         <div className="col d-flex justify-content-center align-items-center">
-                                            <button type='submit' className='btn btn-sm btn-muted p-1 m-2'> Google Search</button>
+                                            <button type='submit' onClick={this.handleSubmit} className='btn btn-sm btn-muted p-1 m-2'> Google Search</button>
                                             <button type='button' className='btn btn-sm btn-muted p-1 m-2'>I'm Feeling Lucky</button>
                                         </div>
                                     </div>
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -5,7 +5,7 @@ export default class NavBar extends React.Component {
         return(
             <nav className="navbar navbar-expand-lg bg-light navbar-light">
                 <button className="navbar-toggler" type='button' data-toggle='collapse' data-target='#navbarSupportedContent' 
-                aria-controls='navbarSupportedContent'  aria-expand='false' aria-label='Toggle navigation'>
+                aria-controls='navbarSupportedContent'  aria-expanded='false' aria-label='Toggle navigation'>
                     <span className='navbar-toggler-icon'></span>
                 </button>
 
